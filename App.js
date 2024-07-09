6a98b1811bcd51e3aba412fa2b205d2b41063e4a
kitty97311 <kitty311.kagaya@gmail.com>
2024-07-09 15:21:56 +0900

    particle effect added

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Emitter } from 'react-native-particles';
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState('#fff');
+  const [particlePos, setParticlePos] = useState({ x: 200, y: 200 });
 
-  const handleOutsideClick = () => {
+  const handleOutsideClick = (event) => {
+    const { locationX, locationY } = event.nativeEvent;
+    setParticlePos({x: locationX, y: locationY})
     setBackgroundColor(getRandomColor()); // Change the background color when clicking outside
     cur_index = (cur_index) % 3
   };
@@ -21,7 +25,18 @@ export default function App() {
   return (
     <TouchableWithoutFeedback onPress={handleOutsideClick}>
       <View style={[styles.container, { backgroundColor }]}>
+        <Emitter
+          numberOfParticles={50}
+          emissionRate={5}
+          interval={200}
+          particleLife={1500}
+          direction={-90}
+          spread={360}
+          fromPosition={{ x: particlePos.x, y: particlePos.y }}
+          infiniteLoop={true}
+      >
         <Text>Hello there</Text>
+      </Emitter>
       </View>
     </TouchableWithoutFeedback>
   );
